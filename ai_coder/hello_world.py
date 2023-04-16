9b35fd673c8fc6710bfbb327fb0c0d63f8e385b2
Pere Ferrera Bertran <ferrerabertran@gmail.com>
2023-04-16 10:26:06 +0200

    tackle issue

--- ai_coder/hello_world.py
+++ ai_coder/hello_world.py
@@ -1,4 +1,4 @@
 
 ===START===
-print("Hello world! I'm an LLM Automated Coding Assistant and I'm here to create pull requests!")
+print("Hello world! I'm an LLM Automated Coding Assistant and I'm here to create pull requests! Let's make coding easier and more fun with automated pull requests!")
 ===END===
