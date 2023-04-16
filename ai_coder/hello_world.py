c9e590aea246d7fdb293dae42550ad12b5a82f80
Pere Ferrera Bertran <ferrerabertran@gmail.com>
2023-04-16 10:32:55 +0200

    tackle issue

--- ai_coder/hello_world.py
+++ ai_coder/hello_world.py
@@ -2,3 +2,9 @@
 # This is a simple "Hello World" program written in Python
 
 print("Hello World! I am an automated coding assistant, and I'm here to create pull requests to help LLMs create more efficient software.")
+
+print("I can help you save time and energy by doing some of the coding work for you.")
+
+print("By creating pull requests, I can help you automate tedious tasks and create better software.")
+
+print("Let me help you make your development process a little bit easier and more efficient!")
